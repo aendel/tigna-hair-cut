48578e9ac8add54fe16e15a5dbb9079e96285d2d
Francesco Vattiato <fvattiato@movia.co>
2021-05-15 00:20:13 +0200

    feat(api): Add api constants && api types

---
+++ pages/api/api.constants.ts
@@ -0,0 +1,2 @@
+export const DB_NAME = "links_db";
+export const DB_COLLECTION = "links_collection";
---
+++ pages/api/api.types.ts
@@ -0,0 +1,3 @@
+import { WithId } from "mongodb";
+
+export type DB_COLLECTION_OBJECT_TYPE = WithId<{ link: string }>;
